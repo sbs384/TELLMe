73fbf09c7a4cdfe34df68ee01b4a42a8fa4cf113
sbs384 <38495153+sbs384@users.noreply.github.com>
2024-04-17 19:49:22 +0800

    Update model_selection.py

--- model_selection.py
+++ model_selection.py
@@ -14,7 +14,7 @@ import numpy as np
 
 from sklearn.decomposition import PCA
 from tqdm import tqdm
-from selection_methods.utils_model_selection_1 import whitening, random_candidate_sampling, random_intra_mixup_candidate_sampling
+from selection_methods.utils_model_selection import whitening, random_candidate_sampling, random_intra_mixup_candidate_sampling
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
@@ -39,7 +39,7 @@ def obtain_train_embeddings(model, train_data_loader):
 
 
 def dataset_encoding(args):
-    cached_dir = f"./cached_data/{args.dataset}"
+    cached_dir = f"./cached_data_1/{args.dataset}"
     if not os.path.exists(cached_dir):
         os.makedirs(cached_dir)
     plm_name = [s for s in args.model_name_or_path.split('/') if s !=''][-1]
@@ -61,7 +61,7 @@ def dataset_encoding(args):
 
         train_data_loader = torch.utils.data.DataLoader(
             train_dataset,
-            shuffle=True,
+            shuffle=False,
             batch_size=args.batch_size,
             collate_fn=train_dataset.collate_fn)
 
@@ -83,24 +83,38 @@ def dataset_encoding(args):
         torch.save(saved_data, cached_dataset_embedding_file)
     return train_src_embeddings, train_tgt_embeddings, train_src_ids
 
+def get_openai_embeddings(args):
+    cached_dir = f"./cached_data_1/{args.dataset}"
+    cached_dataset_openai_embedding_file_merge = os.path.join(cached_dir, 'openai_embedding')
+    _dict = torch.load(cached_dataset_openai_embedding_file_merge)
+    openai_src_embeddings = _dict["src_embeddings"]
+    openai_tgt_embeddings = _dict["tgt_embeddings"]
+    return openai_src_embeddings, openai_tgt_embeddings
 
+def get_openai_lables(src_embeddings, candidate_embeddings, args):
+    src_embeddings /= np.linalg.norm(src_embeddings, axis=2, keepdims=True)
+    candidate_embeddings /= np.linalg.norm(candidate_embeddings, axis=2, keepdims=True)
+    matching_scores = np.sum(src_embeddings * candidate_embeddings, axis = 2)
+    matching_scores = (matching_scores + 1) / 2
+    labels = ((matching_scores) / (1e-8 + np.sum(matching_scores, axis=1, keepdims=True)))
+    labels = np.concatenate(labels, 0)
+    return labels
 
 
 def main(args: argparse.Namespace):
     # set device
     args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+    
+    openai_embeddings_dict = {}
     for model_name_or_path in args.model_name_or_paths:
         args.model_name_or_path = model_name_or_path
         embeddings_dict = {}
-        soft_lb_emb_dict = {}
 
         train_src_embeddings, train_tgt_embeddings, train_src_ids = dataset_encoding(args)
-        print(train_src_embeddings.shape)
-        print(train_tgt_embeddings.shape)
         whitened_train_src_embeddings, whitened_train_tgt_embeddings = whitening(np.copy(train_src_embeddings), np.copy(train_tgt_embeddings))
-        
-        '''
+        openai_src_embeddings, openai_tgt_embeddings = get_openai_embeddings(args)
+        whitened_openai_src_embeddings, whitened_openai_tgt_embeddings = whitening(np.copy(openai_src_embeddings), np.copy(openai_tgt_embeddings))
+
         for candidate_size in args.all_candidate_sizes:
             args.candidate_size = str(candidate_size)
             
@@ -135,14 +149,17 @@ def main(args: argparse.Namespace):
                     from selection_methods.pactran import PACTran as TransMetric
                 elif args.method.startswith("NLEEP"):
                     from selection_methods.nleep import NLEEP as TransMetric
-                elif args.method.startswith("PiLogME"):
-                    from selection_methods.pilogme import LogME as TransMetric
+                elif args.method.startswith("EaSe"):
+                    from selection_methods.ease import LogME as TransMetric
+                elif args.method.startswith("LSLogME"):
+                    from selection_methods.softlogme import LogME as TransMetric
+                elif args.method.startswith("LRLogME"):
+                    from selection_methods.logme_lr import LogME as TransMetric
 
                 if eval(args.save_results):
-                    save_dir = f"output/{args.dataset}/model_selection"
-                    if not os.path.exists(save_dir):
-                        os.makedirs(save_dir)
-                    results_file = f"{save_dir}/{args.method}.json"
+                    if not os.path.exists(args.save_dir):
+                        os.makedirs(args.save_dir)
+                    results_file = f"{args.save_dir}/{args.method}.json"
                     if os.path.exists(results_file):
                         with open(results_file, "r") as f:
                             results_dict = json.load(f)
@@ -164,56 +181,41 @@ def main(args: argparse.Namespace):
                         
                         metric = TransMetric(args)
 
-                        if args.method.startswith("MixupLogME"):
-                            score, score_time = metric.score(np.copy(whitened_train_src_embeddings) if "whitening" in args.method else np.copy(train_src_embeddings),
-                                                             np.copy(whitened_train_tgt_embeddings) if "whitening" in args.method else np.copy(train_tgt_embeddings),
-                                                             np.copy(train_src_ids))
-                        else:
-                            embedding_key = f"whitening_random_{args.seed}_{args.candidate_size}" if "whitening" in args.method else f"random_{args.seed}_{args.candidate_size}"
-                            
-                            fl_save_dir = f"./emb_sampling/{args.dataset}/emb_dict"
-                            st_save_dir = f"./emb_sampling/{args.dataset}/soft_lb_emb_dict"
-                            if not os.path.exists(fl_save_dir):
-                                os.makedirs(fl_save_dir)
-                            if not os.path.exists(st_save_dir):
-                                os.makedirs(st_save_dir)
-                            fl_save_file = fl_save_dir + f"/{embedding_key}"
-                            st_save_file = st_save_dir + f"/{embedding_key}"
-                            
-
-                            if embedding_key in embeddings_dict:
-                                features, labels = embeddings_dict[embedding_key]
-                                if args.method.startswith("PiLogME"):
-                                    repeated_src_embeddings, sampled_candidate_embeddings = random_candidate_sampling(np.copy(whitened_train_src_embeddings) if "whitening" in args.method else np.copy(train_src_embeddings), 
-                                                                                                                  np.copy(whitened_train_tgt_embeddings) if "whitening" in args.method else np.copy(train_tgt_embeddings), 
-                                                                                                                  np.copy(train_src_ids),
-                                                                                                                  args)
-                                    repeated_src_embeddings = np.array(repeated_src_embeddings)
-                                    sampled_candidate_embeddings = np.array(sampled_candidate_embeddings)
-                            else:
-                                repeated_src_embeddings, sampled_candidate_embeddings = random_candidate_sampling(np.copy(whitened_train_src_embeddings) if "whitening" in args.method else np.copy(train_src_embeddings), 
-                                                                                                                  np.copy(whitened_train_tgt_embeddings) if "whitening" in args.method else np.copy(train_tgt_embeddings), 
-                                                                                                                  np.copy(train_src_ids),
-                                                                                                                  args)
-                                features = (repeated_src_embeddings * sampled_candidate_embeddings).reshape(-1, repeated_src_embeddings.shape[-1])
-                                
-                                labels = []
-                                for i in range(repeated_src_embeddings.shape[0]):
-                                    labels.extend([1] + [0]*(int(args.candidate_size)-1))
-                                labels = np.array(labels)
-                                embeddings_dict[embedding_key] = [features, labels]
-                                #torch.save(fl_list, fl_save_file)
-                                
-                                if args.method.startswith("PiLogME"):
-                                    repeated_src_embeddings = np.array(repeated_src_embeddings)
-                                    sampled_candidate_embeddings = np.array(sampled_candidate_embeddings)
                 
-                                #torch.save(st_list, st_save_file)
-                      
-                            if args.method.startswith("PiLogME"):
-                                score, score_time = metric.score(np.copy(features), np.copy(repeated_src_embeddings), np.copy(sampled_candidate_embeddings))
-                            else:
-                                score, score_time = metric.score(np.copy(features), np.copy(labels))
+                        embedding_key = f"whitening_random_{args.seed}_{args.candidate_size}" if "whitening" in args.method else f"random_{args.seed}_{args.candidate_size}"
+                            
+                        #model_emmbeddings
+                        if embedding_key in embeddings_dict:
+                            features, labels = embeddings_dict[embedding_key]
+                        else:
+                            repeated_src_embeddings, sampled_candidate_embeddings = random_candidate_sampling(np.copy(whitened_train_src_embeddings) if "whitening" in args.method else np.copy(train_src_embeddings), 
+                                                                                                                  np.copy(whitened_train_tgt_embeddings) if "whitening" in args.method else np.copy(train_tgt_embeddings), 
+                                                                                                                  np.copy(train_src_ids),
+                                                                                                                  args)
+                            features = (repeated_src_embeddings * sampled_candidate_embeddings).reshape(-1, repeated_src_embeddings.shape[-1])
+                                
+                            labels = []
+                            for i in range(repeated_src_embeddings.shape[0]):
+                                labels.extend([1] + [0]*(int(args.candidate_size)-1))
+                            labels = np.array(labels)
+                            embeddings_dict[embedding_key] = [features, labels]
+                              
+                        #openai_embeddings
+                        if embedding_key in openai_embeddings_dict:
+                            openai_labels = openai_embeddings_dict[embedding_key]
+                        else:
+                            openai_repeated_src_embeddings, openai_sampled_candidate_embeddings = random_candidate_sampling(np.copy(whitened_openai_src_embeddings) if "whitening" in args.method else np.copy(openai_src_embeddings), 
+                                                                                                                      np.copy(whitened_openai_tgt_embeddings) if "whitening" in args.method else np.copy(openai_tgt_embeddings), 
+                                                                                                                      np.copy(train_src_ids), 
+                                                                                                                      args)
+                            openai_labels = get_openai_lables(openai_repeated_src_embeddings, openai_sampled_candidate_embeddings, args)
+                            openai_labels = np.array(openai_labels)
+                            openai_embeddings_dict[embedding_key] = openai_labels
+                                    
+                        if args.method.startswith("EaSe"):
+                            score, score_time = metric.score(np.copy(features), np.copy(openai_labels))
+                        else:
+                            score, score_time = metric.score(np.copy(features), np.copy(labels))
                     
                         all_scores.append(score)
                         all_times.append(score_time)
@@ -221,20 +223,17 @@ def main(args: argparse.Namespace):
                 if eval(args.save_results):
                     results_dict[args.candidate_size] = results_dict.get(args.candidate_size, dict())
                     results_dict[args.candidate_size][args.model_name_or_path] = {
-                        "all_scores": all_scores,
-                        "all_times": all_times,
+                            "all_scores": all_scores,
+                            "all_times": all_times,
                     }
-                    
                     with open(results_file, "w") as f:
                         json.dump(results_dict, f, indent=4)
-                    
 
                 
                 logging.info(f"all scores: {all_scores}")
                 logging.info(f"all times: {all_times}")
                 # logging.info(f"avg score: {np.mean(all_scores)}, avg time: {round(np.mean(all_times), 4)}s")
                 logging.info("-------------------------------END-------------------------------\n")
-        '''
         
 
 
@@ -246,11 +245,13 @@ if __name__ == '__main__':
     parser.add_argument('--save_results', type=str, nargs='?', help='Whether to save results.')
     parser.add_argument('--model_name_or_paths', nargs='+', help='list of pretrained language model identifiers.')
     parser.add_argument('--cache_dir', type=str, default="")
+    parser.add_argument('--save_dir', type=str, default="")
     parser.add_argument('--overwrite_results', type=str, default="False")
     parser.add_argument("--matching_func", type=str)
     parser.add_argument('--pooler', type=str, nargs='?', help='pooling strategy for sentence classification (default: None)')
     parser.add_argument('--all_candidate_sizes', nargs='+', help='list of candidate sizes.')
     parser.add_argument('--batch_size', type=int, default=64, help='maximum number of sentences per batch (default: 64)')
     parser.add_argument('--seeds', nargs='+', help='list of random seeds')
+    parser.add_argument('--dim', type=int, default=0)
 
     main(parser.parse_args())
