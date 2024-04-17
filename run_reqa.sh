a84e4e8d9a01bf33860ba4599bb51aea70ea638e
sbs384 <38495153+sbs384@users.noreply.github.com>
2024-04-17 17:51:45 +0800

    Update run_reqa.sh

--- run_reqa.sh
+++ run_reqa.sh
@@ -1,31 +1,30 @@
 export  CUDA_VISIBLE_DEVICES=0
-DATASET=scifact
+DATASET=bioasq9b
 POOLER=mean
-BATCH_SIZE=16
+BATCH_SIZE=32
 NUM_EPOCHS=10
-CACHE_DIR=/home/baijun/workspace/models
-for PLM in albert-base-v2 microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext michiyasunaga/BioLinkBERT-base
+CACHE_DIR=/home/lzz/myfile/models
+for PLM in bert-base-uncased dmis-lab/biobert-base-cased-v1.1
 do
-    for LR in 2e-5 3e-5 4e-5 5e-5
+    for LR in 1e-5 2e-5 3e-5 4e-5 5e-5
     do
         SAVE_DIR=./output/${DATASET}/fine_tuning/${PLM#*/}_${POOLER}/bs${BATCH_SIZE}_e${NUM_EPOCHS}_lr${LR}/
         mkdir -p ${SAVE_DIR}
         nohup python3 train_reqa.py \
-            --seeds 0 42 512 2023 20246 \
+            --seeds 0 42 512 2023 20246\
             --main_metric mrr \
             --dataset ${DATASET} \
             --epoch ${NUM_EPOCHS} \
             --batch_size ${BATCH_SIZE} \
-            --model_name_or_path ${PLM} \
             --pooler ${POOLER} \
+            --model_name_or_path ${PLM} \
             --matching_func dot \
             --temperature 1 \
+            --cache_dir ${CACHE_DIR} \
             --learning_rate ${LR} \
             --save_dir ${SAVE_DIR} \
-            --cache_dir ${CACHE_DIR} \
             --rm_saved_model True \
             --save_results True \
             > ${SAVE_DIR}/run.log 2>&1
-    done
+    done 
 done
-
